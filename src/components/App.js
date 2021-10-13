5481f6c20d435850361e67589ab816e5ef6d8837
ahmedsgit <acc.ahmedsazzad@gmail.com>
2021-10-13 12:46:16 +0600

    React project setup and set markoup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="App">
-      <div>Hello world</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Hello world</div>
+        </div>
+    );
 }
 
 export default App;
